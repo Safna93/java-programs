c1cb00669b2fc144fb8adada874049b0fa1f7caf
Safna93 <safnasaf111@gmail.com>
2017-07-14 10:45:33 +0530

    This is my final commit

---
+++ Fantezy Project final/Fantezy/FantezyBackendProject/src/main/java/com/fantezy/FantezyBackendProject/App.java
@@ -0,0 +1,13 @@
+package com.fantezy.FantezyBackendProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
